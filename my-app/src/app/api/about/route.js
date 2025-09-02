b81c6d897eda62836f5ad7a267a6a5843db73be7
Komal Sathvik <mksathvik03@gmail.com>
2025-09-02 18:55:26 +0530

    cookies in next.js

---
+++ my-app/src/app/api/about/route.js
@@ -0,0 +1,13 @@
+export function GET(request) {
+    const theme = request.cookies.get("theme");
+    console.log(theme);
+  return Response.json(
+    { name: "sathvik", route: "about" },
+    {
+      status: 201,
+      headers: {
+        "set-cookie": "theme=dark",
+      },
+    }
+  );
+}
